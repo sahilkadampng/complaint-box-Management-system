d83ffe676468b037138493d6b89bc122e1007925
sahilkadampng <your-github-kadamchaya96@gmail.com>
2026-01-10 18:36:11 +0530

    no

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -7,10 +7,8 @@ interface BreadcrumbProps {
 
 export default function Breadcrumb({ current }: BreadcrumbProps) {
     const navigate = useNavigate();
-
     return (
         <div className="text-sm text-gray-500 flex items-center gap-2 mb-6 mt-2">
-
             {/* Home (Dashboard) */}
             <span
                 onClick={() => navigate("/faculty-dashboard")}
@@ -18,9 +16,7 @@ export default function Breadcrumb({ current }: BreadcrumbProps) {
             >
                 Dashboard
             </span>
-
             <ChevronRight className="h-4 w-4 text-gray-400" />
-
             {/* Current Page */}
             <span className="text-black font-medium">{current}</span>
         </div>
